2536c23ad67de7518b962687f17f33697a571827
Omris-Elda <ed.prickett@hotmail.com>
2020-07-16 16:48:05 +0100

    Adding QACChallenge1.py

---
+++ QACChallenge1.py
@@ -0,0 +1,13 @@
+firstnames = [
+    "Christopher", "John", "Jason", "Arsalan", 
+    "Domenico", "Joshua", "Ryan", "Bradley", 
+    "Javas", "Sithembiso", "Jacob", "Clifford", 
+    "Wasim", "Mohamed", "Tobias", "Christopher", 
+    "Samuel", "Edmund", "Amanda", "Steven", "Diarmuid", 
+    ]
+
+firstnames.append("Luke")
+print(firstnames[4])
+print(firstnames.count("Chris"))
+print(firstnames.count("Christopher"))
+# I'm not sure whether I messed with you by using Christopher instead of Chris so here's both.
